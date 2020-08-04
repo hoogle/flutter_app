49bf6fb4a52ad9e68c31fd7e0486651bd4b7f1c6
Hoogle Wang <hoogle.wang@gmail.com>
2020-08-04 16:51:36 +0800

    Add BoxDecoration example

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'dart:math';
 
 void main() => runApp(MaterialApp(
@@ -8,26 +9,28 @@ void main() => runApp(MaterialApp(
 class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    //debugPaintSizeEnabled = true;
     // TODO: implement build
     return Scaffold(
       appBar: new AppBar(
         title: Text('First App by flutter'),
       ),
-      body: Row(
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.all(10),
-            color: Colors.blue,
-            width: 100.0,
-            height: 60.0,
+      body: Container(
+        width: 300.0,
+        height: 100.0,
+        margin: EdgeInsets.all(20.0),
+        padding: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+            color: Colors.cyan,
+            borderRadius: BorderRadius.circular(10.0)
+        ),
+        child: Text(
+          'container',
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0
           ),
-          Container(
-            margin: EdgeInsets.all(10),
-            color: Colors.red,
-            width: 100.0,
-            height: 60.0,
-          ),
-        ],
+        ),
       ),
     );
   }
@@ -54,15 +57,22 @@ class HomePage2State extends State<HomePage2>{
       body: Row(
         children: <Widget>[
           Container(
-            color: getColor(),
-            width: 200.0,
-            height: 200.0,
+            margin: EdgeInsets.all(10.0),
+            width: 100.0,
+            height: 100.0,
+            decoration: BoxDecoration(
+              color: getColor(),
+              shape: BoxShape.circle
+            ),
           ),
           Container(
-            margin: EdgeInsets.only(left: 3.0),
-            color: getColor(),
+            margin: EdgeInsets.all(10.0),
             width: 200.0,
-            height: 200.0,
+            height: 100.0,
+            decoration: BoxDecoration(
+                color: getColor(),
+                borderRadius: BorderRadius.circular(8.0)
+            ),
           ),
         ],
       ),
